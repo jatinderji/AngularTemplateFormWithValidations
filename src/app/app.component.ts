5f9cd47c3b0b7a6c6c5d97ee093bb690562c5316
Jatinder Verma <45707346+jatinderji@users.noreply.github.com>
2021-11-17 17:19:08 +0530

    Angular Template Form along with Validations on fields
    Angular Template Form along with Validations on fields

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularTemplateForm';
+
+  getData(data:any){
+    this.title = data;
+  }
+}
